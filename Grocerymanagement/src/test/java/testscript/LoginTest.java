48a4c8fd48ff831bcb435bf7b773eac5f267d5e7
user <user@REMYA-PC>
2026-01-08 13:28:38 +0530

    commit and push

--- Grocerymanagement/src/test/java/testscript/LoginTest.java
+++ Grocerymanagement/src/test/java/testscript/LoginTest.java
@@ -1,15 +1,20 @@
 package testscript;
+import java.io.IOException;
+
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.LoginPage;
+import utility.ExcelUtility;
 
 public class LoginTest extends Base {	
 @Test	
-public void checkUsesrisAbleToLoginWithValidCredentials()	
+public void checkUsesrisAbleToLoginWithValidCredentials() throws IOException	
 {
 	
-	String user="admin";
-	String pword="admin";
+	//String user="admin";
+	//String pword="admin";
+	String user=ExcelUtility.getStringData(1,0,"LOGINPAGE");//LOGINPAGE IS SHEET NAME
+	String pword=ExcelUtility.getStringData(1,1,"LOGINPAGE");
 	LoginPage loginpage=new LoginPage(driver);
 	loginpage.enterUserName(user);
 	loginpage.enterPassword(pword);
